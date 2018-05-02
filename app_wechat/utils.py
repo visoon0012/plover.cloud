3a18a2c48aa90d98cba6463bc8a5d491c77ad9d0
visoon0012@hotmail.com <visoon0012@hotmail.com>
2018-05-02 15:40:36 +0800

    更新 - 微信服务

--- app_wechat/utils.py
+++ app_wechat/utils.py
@@ -14,13 +14,13 @@ def handle_message(msg):
     # 获取消息内容
     print(msg)
     # 存储
-    with User.objects.get(wechat_openid=msg.FromUserName) as from_user:
-        from_user.message = msg.Content
+    with User.objects.get(wechat_openid=msg['FromUserName']) as from_user:
+        from_user.message = msg['Content']
         from_user.save()
         print(from_user)
     # 分析
     try:
-        wechat_req = WechatRequest.objects.get(message=msg.Content)
+        wechat_req = WechatRequest.objects.get(message=msg['Content'])
         return create_reply(wechat_req.response.message, msg)
     except ObjectDoesNotExist as e:
         defualt = """
@@ -28,4 +28,4 @@ def handle_message(msg):
         http://www.plover.cloud/wechat.html?openid={}\n
         这是您的专属链接，请不要告诉他人。
         """
-        return create_reply(defualt.format(msg.FromUserName), msg)
+        return create_reply(defualt.format(msg['FromUserName']), msg)
