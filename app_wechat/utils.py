cf6866ca1275c26238817b8a1e7f4361961c216d
visoon0012@hotmail.com <visoon0012@hotmail.com>
2018-05-02 15:48:22 +0800

    更新 - 微信服务

--- app_wechat/utils.py
+++ app_wechat/utils.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ObjectDoesNotExist
 from wechatpy import create_reply
+from wechatpy.messages import TextMessage
 
 from app_user.models import User
 from app_wechat.models import WechatRequest
@@ -12,19 +13,13 @@ def handle_message(msg):
     :return:
     """
     # 获取消息内容
-    print(msg)
-    print(type(msg))
-    print(type(msg.type))
-    print(msg.OrderedDict())
-    print(msg.OrderedDict()['FromUserName'])
     # 存储
-    with User.objects.get(wechat_openid=msg['FromUserName']) as from_user:
-        from_user.message = msg['Content']
+    with User.objects.get(wechat_openid=msg.source) as from_user:
+        from_user.message = msg.content
         from_user.save()
-        print(from_user)
     # 分析
     try:
-        wechat_req = WechatRequest.objects.get(message=msg['Content'])
+        wechat_req = WechatRequest.objects.get(message=msg.content)
         return create_reply(wechat_req.response.message, msg)
     except ObjectDoesNotExist as e:
         defualt = """
@@ -32,4 +27,4 @@ def handle_message(msg):
         http://www.plover.cloud/wechat.html?openid={}\n
         这是您的专属链接，请不要告诉他人。
         """
-        return create_reply(defualt.format(msg['FromUserName']), msg)
+        return create_reply(defualt.format(msg.source), msg)
