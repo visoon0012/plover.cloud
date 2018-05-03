92866691b81d70f8eb10cb38213dd4e5b475f250
visoon0012@hotmail.com <visoon0012@hotmail.com>
2018-05-03 10:30:44 +0800

    更新 - 微信服务

--- app_wechat/utils.py
+++ app_wechat/utils.py
@@ -26,13 +26,14 @@ def handle_message(msg):
         # 分析
         if str(msg.content)[0:2] in ['搜索', ]:
             return handle_cmd(msg)
-    else:
-        try:
-            wechat_req = WechatRequest.objects.get(message=msg.content)
-            return create_reply(wechat_req.response.message, msg)
-        except ObjectDoesNotExist as e:
-            default = """请访问下面链接获得更高级功能：\nhttp://www.plover.cloud/wechat.html?openid={}\n这是您的专属链接，请不要告诉他人。\n如需帮助请输入：帮助。"""
-            return create_reply(default.format(msg.source), msg)
+        else:
+            try:
+                wechat_req = WechatRequest.objects.get(message=msg.content)
+                return create_reply(wechat_req.response.message, msg)
+            except ObjectDoesNotExist as e:
+                pass
+    default = """请访问下面链接获得更高级功能：\nhttp://www.plover.cloud/wechat.html?openid={}\n这是您的专属链接，请不要告诉他人。\n如需帮助请输入：帮助。"""
+    return create_reply(default.format(msg.source), msg)
 
 
 def handle_cmd(msg):
