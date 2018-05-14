ac152db6e6a0c50ce6e396ca540d562bf101969b
visoon0012@hotmail.com <visoon0012@hotmail.com>
2018-05-14 10:32:05 +0800

    更新 -

--- app_movie/views.py
+++ app_movie/views.py
@@ -31,7 +31,7 @@ class MovieSimpleViewset(mixins.RetrieveModelMixin, mixins.ListModelMixin, views
 
     @action(methods=['GET'], detail=False)
     def status(self, request):
-        return Response({'status': True, 'status_2': True})
+        return Response({'status': True})
 
     @action(methods=['GET'], detail=False)
     def auto(self, request):
