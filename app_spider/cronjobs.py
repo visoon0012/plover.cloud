5feed0207891e326b123644a4457e681f3848518
visoon0012@hotmail.com <visoon0012@hotmail.com>
2018-06-06 10:45:42 +0800

    更新定时任务

---
+++ app_spider/cronjobs.py
@@ -0,0 +1,111 @@
+import json
+import random
+import logging
+import uuid
+from urllib import parse
+
+import datetime
+from bs4 import BeautifulSoup
+from django.db.models import Min
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from app_movie.models import MovieResource
+from app_spider.models import UrlResource
+from libs.spider.movie_spider import dygang, _6vhao, btbtdy, dy2018, piaohua
+from libs.spider.proxies import xicidaili
+
+logger = logging.getLogger(__name__)
+
+
+def get_resource_urls():
+    # 1.获取代理
+    # 2.爬取几大电影网站首页，把首页URL加入待爬取列表中
+    urls = ['btbtdy.com', 'dygang.com', 'dy2018.com', 'piaohua.com', ]
+    count = 1
+    for url in urls:
+        items = []
+        try:
+            if "dygang" in url:
+                items = dygang.processing_index()
+            elif "dy2018" in url:
+                items = dy2018.processing_index()
+            elif "piaohua" in url:
+                items = piaohua.processing_index()
+            elif "btbtdy" in url:
+                items = btbtdy.processing_index()
+            else:
+                return HttpResponse('没有可以处理的方法', content_type="application/json", status=400)
+        except Exception as e:
+            pass
+        #
+        for item in items:
+            try:
+                url_resource = UrlResource(**item)
+                url_resource.uuid = uuid.uuid3(uuid.NAMESPACE_URL, item['href'])
+                url_resource.source = url
+                url_resource.save()
+                count += 1
+            except Exception as e:
+                pass
+    return count
+
+
+def get_resources():
+    # 2.从待爬取数据库中获得待爬取链接。
+    now = datetime.datetime.now()
+    # start = now - datetime.timedelta(hours=23, minutes=59, seconds=59)
+    # start = now - datetime.timedelta(hours=24*30)
+    # 搜索 错误次数小于10次 且 最后更新时间在30天前的电影
+    urs = UrlResource.objects.filter(error_times__lte=10)
+    # urs = UrlResource.objects.filter(error_times__lte=10, updated_time__lte=start)
+    # 搜索搜索次数最少的URL
+    # spider_times = urs_set.aggregate(Min('spider_times'))
+    # urs = urs_set.filter(spider_times=spider_times['spider_times__min'])
+    # 3.爬取信息，存放数据库
+    logger.warning(len(urs))
+    for ur in urs:
+        # 分析
+        result = []
+        try:
+            if "dygang" in ur.source:
+                result = dygang.processing_detail(ur.href)
+            elif "dy2018" in ur.source:
+                result = dy2018.processing_detail(ur.href)
+            elif "6vhao" in ur.source:
+                result = _6vhao.processing_detail(ur.href)
+            elif "btbtdy" in ur.source:
+                result = btbtdy.processing_detail(ur.href)
+            elif "piaohua" in ur.source:
+                result = piaohua.processing_detail(ur.href)
+        except Exception as e:
+            ur.error_times += 1
+            pass
+        # 保存到数据库
+        new_count = 0
+        for item in result:
+            try:
+                movie_resource = MovieResource.objects.create(
+                    name=parse.unquote(item['name'][:255] if len(item['name']) > 255 else item['name']),
+                    title=parse.unquote(ur.title),
+                    download_link=parse.unquote(item['download_link']),
+                    download_uuid=uuid.uuid3(uuid.NAMESPACE_URL, parse.unquote(item['download_link'])),
+                    source=parse.unquote(item['source']),
+                    source_type=ur.source_type
+                )
+                movie_resource.save()
+                new_count += 1
+            except Exception as e:
+                movie_resource = MovieResource.objects.get(download_uuid=uuid.uuid3(uuid.NAMESPACE_URL, parse.unquote(item['download_link'])))
+                movie_resource.source_type = ur.source_type
+                movie_resource.save()
+                pass
+        # 保存搜索次数
+        ur.spider_times += 1
+        # 判断是否有新增，没有新增错误次数+1，有资源错误次数清零
+        if new_count == 0:
+            ur.error_times += 1
+        else:
+            ur.error_times = 0
+        ur.save()
+        logger.warning('处理：%s，数据：%s，新增：%s' % (ur.href, len(result), new_count))
--- config/settings.py
+++ config/settings.py
@@ -88,9 +88,9 @@ CORS_ORIGIN_WHITELIST = (
 
 # 定时任务
 CRONJOBS = [
-    ('0 1 * * *', 'app_spider.utils.get_resource_urls', '>>/www/spider_resource.log'),
-    ('0 2 * * *', 'app_spider.utils.get_resources', '>>/www/spider_resource.log'),
-    ('0 3 * * *', 'app_movie.utils.auto_get_movie_simple', '>>/www/spider_movie.log'),
+    ('0 11 * * *', 'app_spider.cronjobs.get_resource_urls', '>>/www/spider_resource.log'),
+    ('0 12 * * *', 'app_spider.cronjobs.get_resources', '>>/www/spider_resource.log'),
+    ('0 13 * * *', 'app_movie.cronjobs.auto_get_movie_simple', '>>/www/spider_movie.log'),
     # ('0 */4 * * *', 'app_book.spiders.auto_update_fork', '>>/root/plover.cloud/logs/auto_update_fork.log'),
     # ('0 4 * * *', 'app_book.spiders.auto_download', '>>/root/plover.cloud/logs/auto_download.log'),
 ]
